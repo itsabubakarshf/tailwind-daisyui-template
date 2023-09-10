025c4ba55d4fccfd208971d68338f39145891cf9
itsabubakarshf <ItsAbubakarShf@gmail.com>
2023-09-10 22:45:48 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const App = () => {
+  return (
+    <React.Fragment>
+      <button class="btn bg-primary">Button</button>
+    </React.Fragment>
+  )
+}
+
+export default App
